2a074e17e8ab71b7bc1fca1ae7c4d961ac9a727a
Hallmarkbooks <businesshallmarkbooks@gmail.com>
2024-09-06 08:31:27 -0700

    Update urls.py

--- happ/urls.py
+++ happ/urls.py
@@ -9,7 +9,4 @@ urlpatterns = [
     path('sendmail/', views.contact, name= 'contact'),
     path('order/', views.order_view, name='order'),
 
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
