2f96491a7adddb73ae9b2d32df3d8c5fcad5ef51
Hallmarkbooks <businesshallmarkbooks@gmail.com>
2024-09-06 08:30:41 -0700

    Update urls.py

--- hproject/urls.py
+++ hproject/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('', include('happ.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
